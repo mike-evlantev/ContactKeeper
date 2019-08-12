127e5b960448af2894ad6a49d3019de50e6d92df
Mike Evlantev <mike.evlantev@questanalytics.com>
2019-08-12 14:30:48 -0700

    added password encryption, save user to db

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/User");
 
@@ -19,13 +20,31 @@ router.post(
       "Password must be at least 8 characters in length"
     ).isLength({ min: 8 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("OK!");
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+      user = new User({ name, email, password });
+
+      //Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save to DB
+      await user.save();
+      res.send("User saved");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
