ae4631f63c9073a54cbfe244ef953d164527752a
Mike Evlantev <mike.evlantev@questanalytics.com>
2019-08-13 14:50:19 -0700

    added router to main app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import "./App.css";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 };
 
