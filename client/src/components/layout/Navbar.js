7a241ca810a18eb9e4fd5d22151af9c07ad59075
Mike Evlantev <mike.evlantev@questanalytics.com>
2019-08-14 18:23:11 -0700

    refactored navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,14 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ title, icon }) => {
   return (
-    <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand" href="www.google.com">
-        <i className={icon} />
-        &nbsp;
-        {title}
-      </a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="www.google.com">
+          <i className={icon} />
+          &nbsp;
+          {title}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
