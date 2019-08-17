a6cca1c4fbf739a55a408c75013956decdf469f6
Mike Evlantev <mikeev21@gmail.com>
2019-08-17 09:33:14 -0700

    removing node_modules

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,7 @@ router.get("/", auth, async (req, res) => {
     res.json(user);
   } catch (error) {
     console.error(error.message);
-    res.status(500).send("Server error");
+    res.status(500).send("Server Error");
   }
 });
 
