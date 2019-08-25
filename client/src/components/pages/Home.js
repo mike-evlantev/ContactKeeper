bdb1fdea80cdab6731be6598a36d3b7cca9e148a
Mike Evlantev <mikeev21@gmail.com>
2019-08-25 11:20:40 -0700

    columnized card layout

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Contacts from "../contacts/Contacts";
 import ContactForm from "../contacts/ContactForm";
+import ContactFormOriginal from "../contacts/ContactFormOriginal";
 
 const Home = () => {
   return (
-    <div>
-      <div>
+    <div className="row">
+      <div className="col-sm-6">
         <ContactForm />
+        {/* <ContactFormOriginal /> */}
       </div>
-      <div>
+      <div className="col-sm-6">
         <Contacts />
       </div>
     </div>
