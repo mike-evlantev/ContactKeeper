d4df1aebf11286f291544d796cff8bcb94feb2d5
Mike Evlantev <mikeev21@gmail.com>
2019-08-25 11:22:22 -0700

    card items layout and logic updates

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -2,33 +2,43 @@ import React, { useState, useContext, useEffect } from "react";
 import ContactContext from "../../context/contact/contactContext";
 
 const ContactForm = () => {
-  const clearedContact = {
-    name: "",
-    email: "",
-    phone: "",
-    type: "personal"
-  };
   const contactContext = useContext(ContactContext);
-  const [contact, setContact] = useState(clearedContact);
 
-  const { name, email, phone, type } = contact;
-  const { addContact, clearCurrent, current } = contactContext;
+  const { addContact, updateContact, clearCurrent, current } = contactContext;
 
   useEffect(() => {
     if (current !== null) {
       setContact(current);
     } else {
-      setContact(clearedContact);
+      setContact({
+        name: "",
+        email: "",
+        phone: "",
+        type: "personal"
+      });
     }
-  }, [contactContext, current, clearedContact]);
+  }, [contactContext, current]);
+
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    type: "personal"
+  });
+
+  const { name, email, phone, type } = contact;
 
   const onChange = e =>
     setContact({ ...contact, [e.target.name]: e.target.value });
 
   const onSubmit = e => {
     e.preventDefault();
-    addContact(contact);
-    setContact(clearedContact);
+    if (current === null) {
+      addContact(contact);
+    } else {
+      updateContact(contact);
+    }
+    clearAll();
   };
 
   const clearAll = () => {
@@ -36,61 +46,78 @@ const ContactForm = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <h4 className="text-primary">{current ? "Edit" : "Add"} Contact</h4>
-      <input
-        type="text"
-        placeholder="Name"
-        name="name"
-        value={name}
-        onChange={onChange}
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        name="email"
-        value={email}
-        onChange={onChange}
-      />
-      <input
-        type="text"
-        placeholder="Phone"
-        name="phone"
-        value={phone}
-        onChange={onChange}
-      />
-      <h5>Conatct Type</h5>
-      <input
-        type="radio"
-        name="type"
-        value="personal"
-        checked={type === "personal"}
-        onChange={onChange}
-      />{" "}
-      Personal{" "}
-      <input
-        type="radio"
-        name="type"
-        value="professional"
-        checked={type === "professional"}
-        onChange={onChange}
-      />{" "}
-      Professional{" "}
-      <div>
-        <input
-          type="submit"
-          value={current ? "Update Contact" : "Add Contact"}
-          className="btn btn-primary btn-block"
-        />
+    <div className="card">
+      <div class="card-header">{current ? "Edit" : "Add"} Contact</div>
+      <div className="card-body">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Phone"
+              name="phone"
+              value={phone}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <div className="form-check">
+              <label className="form-check-label mr-3">Contact Type</label>
+              <input
+                type="radio"
+                name="type"
+                value="personal"
+                checked={type === "personal"}
+                onChange={onChange}
+              />{" "}
+              Personal{" "}
+              <input
+                type="radio"
+                name="type"
+                value="professional"
+                checked={type === "professional"}
+                onChange={onChange}
+              />{" "}
+              Professional
+            </div>
+          </div>
+          <div>
+            <input
+              type="submit"
+              value={current ? "Update Contact" : "Add Contact"}
+              className="btn btn-primary float-right"
+            />
+          </div>
+          {current && (
+            <div>
+              <button className="btn btn-light" onClick={clearAll}>
+                Clear
+              </button>
+            </div>
+          )}
+        </form>
       </div>
-      {current && (
-        <div>
-          <button className="btn btn-light btn-block" onClick={clearAll}>
-            Clear
-          </button>
-        </div>
-      )}
-    </form>
+    </div>
   );
 };
 
--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -12,41 +12,57 @@ const ContactItem = ({ contact }) => {
   };
 
   return (
-    <div className="card bg-light">
-      <h5 className="text-primary text-left">
-        {name}{" "}
+    <div
+      className={
+        "card " +
+        (type === "professional" ? "border-success" : "border-primary") +
+        " mb-3"
+      }
+    >
+      <div
+        className={
+          "card-header " +
+          (type === "professional" ? "border-success" : "border-primary") +
+          " bg-transparent"
+        }
+      >
+        {name}
         <span
           className={
             "badge " +
-            (type === "professional" ? "badge-success" : "badge-primary")
+            (type === "professional" ? "badge-success" : "badge-primary") +
+            " float-right"
           }
         >
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
-      </h5>
-      <ul className="list">
-        {email && (
-          <li>
-            <i className="fas fa-envelope-open" /> {email}
-          </li>
-        )}
-        {phone && (
-          <li>
-            <i className="fas fa-phone" /> {phone}
-          </li>
-        )}
-      </ul>
-      <p>
-        <button
-          className="btn btn-dark btn-sm"
-          onClick={() => setCurrent(contact)}
-        >
-          Edit
-        </button>
-        <button className="btn btn-danger btn-sm" onClick={onDelete}>
-          Delete
-        </button>
-      </p>
+      </div>
+      <div className="card-body">
+        <h5 className="text-primary text-left"></h5>
+        <ul className="list">
+          {email && (
+            <li>
+              <i className="fas fa-envelope-open" /> {email}
+            </li>
+          )}
+          {phone && (
+            <li>
+              <i className="fas fa-phone" /> {phone}
+            </li>
+          )}
+        </ul>
+        <p className="float-right">
+          <button
+            className="btn btn-dark btn-sm mr-1"
+            onClick={() => setCurrent(contact)}
+          >
+            Edit
+          </button>
+          <button className="btn btn-danger btn-sm" onClick={onDelete}>
+            Delete
+          </button>
+        </p>
+      </div>
     </div>
   );
 };
