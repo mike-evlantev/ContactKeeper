0d70a83502f30491198a499a9d18027d1f65f841
Mike Evlantev <mikeev21@gmail.com>
2019-08-25 21:35:06 -0700

    clean up

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Contacts from "../contacts/Contacts";
 import ContactForm from "../contacts/ContactForm";
-import ContactFormOriginal from "../contacts/ContactFormOriginal";
 
 const Home = () => {
   return (
     <div className="row">
       <div className="col-sm-6">
         <ContactForm />
-        {/* <ContactFormOriginal /> */}
       </div>
       <div className="col-sm-6">
         <Contacts />
