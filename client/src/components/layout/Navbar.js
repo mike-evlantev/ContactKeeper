519a3ff8a35c9a3fcfc706588541c2d7765dcac9
Mike Evlantev <mikeev21@gmail.com>
2019-08-26 21:01:39 -0700

    dressed up Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,11 +6,11 @@ const Navbar = ({ title, icon }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <a className="navbar-brand" href="www.google.com">
+        <Link className="navbar-brand" to="/">
           <i className={icon} />
           &nbsp;
           {title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -23,12 +23,16 @@ const Navbar = ({ title, icon }) => {
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <Link to="/">Home</Link>
-            </li>
+          <ul className="navbar-nav ml-auto">
+            {/* <li className="nav-item active">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li> */}
             <li className="nav-item">
-              <Link to="/about">About</Link>
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
             </li>
           </ul>
         </div>
