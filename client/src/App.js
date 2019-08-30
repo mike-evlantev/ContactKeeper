7609ea44b8afcf9ce5aeb9f898b3f56d42052503
Mike Evlantev <mikeev21@gmail.com>
2019-08-29 20:30:44 -0700

    login register components and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -5,25 +5,35 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Footer from "./components/layout/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container mt-5">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-          <Footer />
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container mt-5">
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+              <Footer />
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 };
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -34,6 +34,16 @@ const Navbar = ({ title, icon }) => {
                 About
               </Link>
             </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">
+                Register
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
