bc69d0b2b7b38acb77585ba4eabe72aa48d8f586
Mike Evlantev <mikeev21@gmail.com>
2019-09-01 10:16:19 -0700

    added auth context, redirect on register, alerts, connected register() to backend

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import AlertContext from "../../context/alert/alertContext";
+import AuthContext from "../../context/auth/authContext";
 
-const Register = () => {
+const Register = props => {
   const { setAlert } = useContext(AlertContext);
+  const { register, error, clearErrors, isAuthenticated } = useContext(
+    AuthContext
+  );
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -18,9 +22,24 @@ const Register = () => {
     } else if (password !== password2) {
       setAlert("Passwords do not match", "danger");
     } else {
-      console.log("Register submit");
+      register({
+        name,
+        email,
+        password
+      });
     }
   };
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push("/");
+    }
+    if (error === "User already exists") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
 
   return (
     <div className="form-container">
