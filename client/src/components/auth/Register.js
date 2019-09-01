6d34ae0acb6bb83dd355861a8765c067a4f55b25
Mike Evlantev <mikeev21@gmail.com>
2019-08-31 19:36:53 -0700

    Register component alerts

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
 
 const Register = () => {
+  const { setAlert } = useContext(AlertContext);
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -11,7 +13,13 @@ const Register = () => {
   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
   const onSubmit = e => {
     e.preventDefault();
-    console.log("Register submit");
+    if (name === "" || email === "" || password === "") {
+      setAlert("Please enter all fields", "danger");
+    } else if (password !== password2) {
+      setAlert("Passwords do not match", "danger");
+    } else {
+      console.log("Register submit");
+    }
   };
 
   return (
@@ -21,15 +29,15 @@ const Register = () => {
       </h1>
       <form onSubmit={onSubmit}>
         <div className="form-group">
-          <lable htmlFor="name">Name</lable>
+          <label htmlFor="name">Name</label>
           <input type="text" name="name" value={name} onChange={onChange} />
         </div>
         <div className="form-group">
-          <lable htmlFor="email">Email</lable>
+          <label htmlFor="email">Email</label>
           <input type="email" name="email" value={email} onChange={onChange} />
         </div>
         <div className="form-group">
-          <lable htmlFor="password">Password</lable>
+          <label htmlFor="password">Password</label>
           <input
             type="password"
             name="password"
@@ -38,7 +46,7 @@ const Register = () => {
           />
         </div>
         <div className="form-group">
-          <lable htmlFor="password2">Confirm password</lable>
+          <label htmlFor="password2">Confirm password</label>
           <input
             type="password"
             name="password2"
