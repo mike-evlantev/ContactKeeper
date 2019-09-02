c7e25f836df90416aae894c239def7bcd9f36577
Mike Evlantev <mikeev21@gmail.com>
2019-09-01 20:09:42 -0700

    login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,15 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import AuthContext from "../../context/auth/authContext";
+import AlertContext from "../../context/alert/alertContext";
 
-const Login = () => {
+const Login = props => {
   const [user, setUser] = useState({
     email: "",
     password: ""
   });
+  const { setAlert } = useContext(AlertContext);
+  const { login, error, clearErrors, isAuthenticated } = useContext(
+    AuthContext
+  );
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push("/");
+    }
+    if (error === "Invalid credentials") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
   const { email, password } = user;
   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
   const onSubmit = e => {
     e.preventDefault();
-    console.log("Login submit");
+    if (email === "" || password === "") {
+      setAlert("Please fill in all fields", "danger");
+    } else {
+      login({ email, password });
+    }
   };
 
   return (
