4ca3c449e3620035a7e962b390c86647f778e6a5
Mike Evlantev <mikeev21@gmail.com>
2019-09-02 20:50:03 -0700

    cleaned up and dressed up Register and Login forms

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -34,26 +34,43 @@ const Login = props => {
   };
 
   return (
-    <div className="form-container">
-      <h1>
-        Account <span className="text-primary">Login</span>
-      </h1>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" value={email} onChange={onChange} />
+    <div className="row">
+      <div className="col"></div>
+      <div className="col">
+        <div className="form-container">
+          <h5 className="text-center">
+            Account <span className="text-primary">Login</span>
+          </h5>
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                placeholder="Email"
+                value={email}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={onChange}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Login"
+              className="btn btn-outline-primary float-right"
+            />
+          </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={onChange}
-          />
-        </div>
-        <input type="submit" value="Login" className="btn btn-primary" />
-      </form>
+      </div>
+      <div className="col"></div>
     </div>
   );
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -42,39 +42,63 @@ const Register = props => {
   }, [error, isAuthenticated, props.history]);
 
   return (
-    <div className="form-container">
-      <h1>
-        Account <span className="text-primary">Register</span>
-      </h1>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" value={name} onChange={onChange} />
+    <div className="row">
+      <div className="col"></div>
+      <div className="col">
+        <div className="form-container">
+          <h5 className="text-center">
+            Account <span className="text-primary">Register</span>
+          </h5>
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                placeholder="Name"
+                value={name}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                placeholder="Email"
+                value={email}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                name="password2"
+                placeholder="Confirm password"
+                value={password2}
+                onChange={onChange}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Register"
+              className="btn btn-outline-primary float-right"
+            />
+          </form>
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" value={email} onChange={onChange} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password2">Confirm password</label>
-          <input
-            type="password"
-            name="password2"
-            value={password2}
-            onChange={onChange}
-          />
-        </div>
-        <input type="submit" value="Register" className="btn btn-primary" />
-      </form>
+      </div>
+      <div className="col"></div>
     </div>
   );
 };
